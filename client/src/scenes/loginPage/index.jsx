83fa6a059dbe788a558ea55678fabc0c47dc1212
Kalp Chaudhary <68579607+KalpChaudhary@users.noreply.github.com>
2022-12-23 14:52:43 -0500

    Delete client directory

--- client/src/scenes/loginPage/index.jsx
+++
@@ -1,9 +0,0 @@
-const LoginPage = () => {
-    return (
-        <div className="home-page">
-            <h1>Login Page</h1>
-        </div>
-    );
-}
-
-export default LoginPage;
--- client/src/scenes/profilePage/index.jsx
+++
@@ -1,9 +0,0 @@
-const ProfilePage = () => {
-    return (
-        <div className="home-page">
-            <h1>ProfilePage</h1>
-        </div>
-    );
-}
-
-export default ProfilePage;
