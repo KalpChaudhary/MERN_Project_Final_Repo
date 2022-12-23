68d7f3cd9af95a64166ef379f37134f4d25683bd
KalpChaudhary <kalp.s.p.123@gmail.com>
2022-12-23 17:34:58 -0500

    Test deleted files

--- client/src/index.js
+++ client/src/index.js
@@ -1,32 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import authReducer from "./state";
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import storage from "redux-persist/lib/storage";
-import { PersistGate } from "redux-persist/integration/react";
-import { version } from "react-dom";
-
-const persistConfig =  {key: 'root',storage, version: 1};
-const persistedReducer = persistReducer(persistConfig, authReducer);
-
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- client/src/scenes/navbar/index.jsx
+++
@@ -1,9 +0,0 @@
-const Navbar = () => {
-    return (
-        <div className="home-page">
-            <h1>Navbar</h1>
-        </div>
-    );
-}
-
-export default Navbar;
