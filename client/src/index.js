3ea00884ea12560b7efab101f17f9b308d6a74c1
KalpChaudhary <kalp.s.p.123@gmail.com>
2022-12-23 13:20:48 -0500

    commit -m "commit check"

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import authReducer from "./state";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+import { version } from "react-dom";
+
+const persistConfig =  {key: 'root',storage, version: 1};
+const persistedReducer = persistReducer(persistConfig, authReducer);
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
- 
--- client/src/state/index.js
+++ client/src/state/index.js
@@ -41,4 +41,5 @@ export const  authSlice = createSlice({
 })
 
 export const  {setMode, setLogin, setLogout, setFriends, setPosts, setPost} = authSlice.actions;
+
 export default  authSlice.reducer;
