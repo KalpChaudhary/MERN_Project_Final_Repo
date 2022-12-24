accf1759c385cd28741a3f5257245d78219a0c38
KalpChaudhary <kalp.s.p.123@gmail.com>
2022-12-23 23:19:00 -0500

    Done for the day

--- client/src/scenes/Navbar/Navbar.jsx
+++ client/src/scenes/Navbar/Navbar.jsx
@@ -1,10 +1,54 @@
+import { useState } from "react";
+import {
+  Box,
+  IconButton,
+  InputBase,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery,
+  Typography,
+  Select,
+} from "@mui/material";
+
+import {
+  Search,
+  Message,
+  DarkMode,
+  LightMode,
+  Notifications,
+  Help,
+  Menu,
+  Close,
+} from "@mui/icons-material";
+
+import { useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "state";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "components/FlexBetween";
+
 const Navbar = () => {
-    return (
-      <div className="navbar">
-        <h1>Navbar</h1>
-      </div>
-    );
-  };
+
+  const [isMobilemenuToggled, setIsMobilemenuToggled] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user  = useSelector(state => state.user);
+  const isMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const theme = useTheme();
   
-  export default Navbar;
+  const neutralLight = theme.palette.neutral.light;
+  const neutralDark = theme.palette.neutral.dark;
+  const background = theme.palette.background.default;
+  const primaryLight = theme.palette.primary.light;
+  const alt = theme.palette.background.alt;
+
+  const fullName = `${user.firstName} ${user.lastName}`;
   
+  return (
+    <div className="navbar">
+      <h1>Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
